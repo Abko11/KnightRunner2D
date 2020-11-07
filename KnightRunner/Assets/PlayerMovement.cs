a32e4ca5b95c6ef7a9642f07af7e4e8239fb9fc0
Abko11 <abko1999@gmail.com>
2020-11-06 20:57:53 -0800

    Player Movement Animations Added
    jumping animation still needs some work (must double click space for it to play)

--- KnightRunner/Assets/PlayerMovement.cs
+++ KnightRunner/Assets/PlayerMovement.cs
@@ -6,6 +6,8 @@ public class PlayerMovement : MonoBehaviour
 {
 
     public CharacterController2D controller;  //references the CharacterController2D script we have already made for our player
+    public Animator animator;  //create a reference variable named animator that will reference the Animator component for the player
+
 
     float horizontalMove = 0f;  //variable for horizontal movement (initialize to 0)
     public float runSpeed = 40f;   //variable to hold run speed (set to public so that we can see/adjust it in the Unity inspector)
@@ -26,12 +28,22 @@ public class PlayerMovement : MonoBehaviour
     {
         horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;  //Input class is for user input such as pressing arrow keys while "Horizontal specifies a value 1 or -1 based on which direction the player moves in (1 for right, -1 for left)
                                                                      //multiply by runSpeed so that it can factor into the player movement based on the input
-       // animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
+       
+        animator.SetFloat("Speed", Mathf.Abs(horizontalMove));  //"Speed" is the name of the parameter in our Animator
+        //we want our Speed variable to always be positive so that our animations will properly transition (since moving left returns negative values)
 
         if (Input.GetButtonDown("Jump")) //checking to see if user presses jump button
         {
             jump = true;  //if jump key is pressed, jump
+            animator.SetBool("isJumping", true);   //use setBool (since "isJumping is a boolean) to make sure animation transitions properly when player jumps
+            //in the character controller, we have a loophole for when the player lands (since as of now there is no way for the game to tell when the player is actually landing)
+            //see the "Events" tab in the character controller component for the player
         }
+    }
+
+    public void OnLanding() //this function is to tell the Animator to stop jumping (public so that "Event" can see this function
+    {
+        animator.SetBool("isJumping", false);
     }
 
     private void FixedUpdate()  //updates a fixed amount of times per second (this is what we want to use to move our character)
